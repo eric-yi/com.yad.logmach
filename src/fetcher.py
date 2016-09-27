f319b1e33b996034f0a733616f5ff17ba99bd574
xbyi <yi_xiaobin@163.com>
2016-09-27 11:17:35 +0800

    feat(fetcher): use bayer to classify log

--- src/fetcher.py
+++ src/fetcher.py
@@ -2,41 +2,155 @@
 # -*- coding:utf-8 -*-
 
 from re import *
+import json
+from numpy import *
 
 def fetch(filename):
     _file = open(filename, 'r')
+    _feature_list = []
+    for _line in _file:
+        _json_str = json.loads(_line)
+        _feature_list.append(_json_str)
+
+    keys_list = []
+    key_statistics_dic = {}
+    for _feature in _feature_list:
+        keys = []
+        for (k, v) in _feature.items():
+            keys.append(k)
+            if k in key_statistics_dic:
+                key_statistics_dic[k] += 1
+            else:
+                key_statistics_dic[k] = 1
+        keys_list.append(keys)
+
+    _statistics_max = 0
+    _statistics_min = 0
+    for (k, v) in key_statistics_dic.items():
+        #print '%s= %d, ' % (k, v)
+        if v > _statistics_max:
+            _statistics_max = v
+        if v < _statistics_min or _statistics_min == 0:
+            _statistics_min = v
+
+    _range = _statistics_max -  _statistics_min
+    #print 'max= %d, min = %d' % (_statistics_max, _statistics_min)
+    _feature_rate = {}
+    for (k, v) in key_statistics_dic.items():
+        _rate = (v - _statistics_min) / float (_range)
+        _feature_rate[k] = _rate
+        #print '%s= %f, ' % (k, _feature_rate[k])
+
+    p1_num, p0_num = training_keys(keys_list, _feature_rate)
+    _p1_total = sum(p1_num)
+    _p0_total = sum(p0_num)
+    _total = _p1_total + _p0_total
+    #print 'total is %d' % (_total)
+
+    _p1_rate = _p1_total / float(_total)
+    _p0_rate = _p0_total / float(_total)
+
+    _doc_num = len(keys_list)
+    _rate_p1 = []
+    _rate_p0 = []
+    for i in range(_doc_num):
+        _p1 = p1_num[i]
+        _p0 = p0_num[i]
+        p1_rate = _p1 / (_p1 + _p0) * _p1_rate / ((_p1 + _p0) / _total)
+        p0_rate = _p0 / (_p1 + _p0) * _p0_rate / ((_p1 + _p0) / _total)
+        _rate_p1.append(p1_rate)
+        _rate_p0.append(p0_rate)
+        #print '%d line: positive rate= %f, negative rate= %f' % (i, p1_rate, p0_rate)
+
+    for i in range(_doc_num):
+        p1_rate = _rate_p1[i] / sum(_rate_p1)
+        p0_rate = _rate_p0[i] / sum(_rate_p0)
+        clazz = 'one'
+        if p1_rate < p0_rate:
+            clazz = 'two'
+        print '%d line: positive= %f, negative= %f, %s' % (i, p1_rate, p0_rate, clazz)
+
+
+
+def training_keys(keys_list, key_rate):
+    _doc_num = len(keys_list)
+    p0_num = zeros(_doc_num)
+    p1_num = zeros(_doc_num)
+    i = 0
+    for keys in keys_list:
+        for key in keys:
+            _rate = key_rate[key]
+            if _rate >= 0.5:
+                p1_num[i] += 1
+            else:
+                p0_num[i] += 1
+        i += 1
+
+    #for i in range(_doc_num):
+    #    print '%d line: positive = %d, negative = %d' % (i, p1_num[i], p0_num[i])
+
+    return p1_num, p0_num
+
+
+def fetch_temp(filename):
+    _file = open(filename, 'r')
     _logs = []
-    _log_item = []
+    _log_items = []
+    _date_pattern = '^\[\d{1,2}/\d{1,2}/\d{2}'
     for _line in _file:
         if len(_line.strip()) > 0:
-            pattern = compile('^\[\d{1,2}/\d{1,2}/\d{2}')
-            if (pattern.match(_line)):
-                if len(_log_item) > 0:
-                    _logs.append(_log_item)
-                    _log_item = []
-            _log_item.append(_line)
-    if len(_log_item) > 0:
-        _logs.append(_log_item)
-
-    #for _log in _logs:
-    #    for _item in _log:
-    #        print _item
-    #    print '-------------------'
+            merge_by_date(_date_pattern, _line, _line, _logs, _log_items)
+    if len(_log_items) > 0:
+        _logs.append(_log_items)
 
     _patterns = [
-        '\[\d{1,2}/\d{1,2}/\d{2}\s\d{1,2}:\d{2}:\d{2}:\d{3}\s\S{0,}\]\s',
-        '\S{0,}\s',
-        '\S{0,}\s',
-        '\S{0,}\s'
+        '\[\d{1,2}/\d{1,2}/\d{2}\s\d{1,2}:\d{2}:\d{2}:\d{3}\s\S*\]\s+',
+        '\w+\s+',
+        '\S+\s+',
+        '\S+\s+'
     ]
+
+    _new_logs = []
     for _log in _logs:
         _log_str = ''.join(_log)
-        print _log_str
         _items = split_log(_patterns, _log_str)
-        for _item in _items:
-            print _item
-        print '--------------------------'
+        _new_logs.append(_items)
 
+    #print_log(_new_logs)
+
+    _date_pattern = '^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d{3}'
+    _log_items = []
+    _logs = []
+    for _log in _new_logs:
+        _message = _log[-1]
+        #print _message
+        merge_by_date(_date_pattern, _message, _log, _logs, _log_items)
+    if len(_log_items) > 0:
+        _logs.append(_log_items)
+
+    print_log(_logs)
+
+    for _log in _logs:
+        for _items in _log:
+            for _item in _items:
+                if '请求参数明文：' in _item:
+                    print _item.split('请求参数明文：')[-1].split('\n')[0]
+
+
+
+def print_log(logs):
+    for log in logs:
+        for item in log:
+            print item
+        print '=============='
+
+def merge_by_date(date_pattern, message, log, logs, items):
+    pattern = compile(date_pattern)
+    if (pattern.match(message)):
+        if len(items) > 0:
+            logs.append(items)
+            items = []
+    items.append(log)
 
 def split_log(patterns, log):
     _result = []
--- test/test_fetcher.py
+++ test/test_fetcher.py
@@ -8,9 +8,14 @@ from re import *
 class TestCase(unittest.TestCase):
     TEST_STR = '[3/16/16 9:42:46:066 CST] 000000b7 SystemOut     O 2016-03-16 09:42:46.066 INFO    c.y.i.d.s.impl.BoseraServiceImpl:277 - BoseraServiceImpl.qryAsset-请求参数明文：{"contractNo":"205ftzex20151229017581","applyId":"ftzexc199d6e5e03047fcabb2c39a132b25d0","opType":"QRY002"}'
 
-    @unittest.skip('skip')
+    #@unittest.skip('skip')
     def test_fetch(self):
-        fetcher.fetch('../logs/00.log')
+        fetcher.fetch('../logs/00_filter.log')
+        self.assertTrue(True)
+
+    @unittest.skip('skip')
+    def test_fetch_temp(self):
+        fetcher.fetch_temp('../logs/00.log')
         self.assertTrue(True)
 
     @unittest.skip('skip')
@@ -18,6 +23,10 @@ class TestCase(unittest.TestCase):
         _data = '[3/16/16 9:42:46:069 CST] 000000b7'
         pattern = compile('^\[\d{1,2}/\d{1,2}/\d{2}')
         self.assertTrue(pattern.match(_data))
+        _data = '2016-03-16 09:42:46.066 INFO    c.y.i.d.s.impl.BoseraServiceImpl:277'
+        pattern = compile('^\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d{3}')
+        self.assertTrue(pattern.match(_data))
+
 
     @unittest.skip('skip')
     def test_split_by_date(self):
@@ -33,7 +42,7 @@ class TestCase(unittest.TestCase):
         print _result[1]
         self.assertEquals(len(_result), 2)
 
-    #@unittest.skip('skip')
+    @unittest.skip('skip')
     def test_split_log(self):
         patterns = [
             '\[\d{1,2}/\d{1,2}/\d{2}\s\d{1,2}:\d{2}:\d{2}:\d{3}\s\S*\]\s+',
